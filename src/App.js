b334e9969d1ec03ada61bb0388f83bd6e4c7c07f
unknown <galaxy.net0710@gmail.com>
2021-09-11 11:53:45 -0500

    Header Component created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 //styles
 import { GlobalStyle } from './GlobalsStyles'
+//styles
+import Header from './components/Header/index';
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       Start here.
       https://www.youtube.com/watch?v=nTeuhbP7wdE
       56:20
+      <Header/>
       <GlobalStyle/>
     </div>
   );
