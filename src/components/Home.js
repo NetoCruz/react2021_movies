b1084d3061d2acbb14c79577970f8d132dae590e
unknown <galaxy.net0710@gmail.com>
2021-09-13 14:43:26 -0500

    Spinner added

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ import { POSTER_SIZE,BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 import HeroImage from "./HeroImage";
 import Grid  from "./Grid"
 import Thumb from "./Thumb"
+import Spinner from "./Spinner";
 //hook
 import {useHomeFetch} from '../hooks/useHomeFetch'
 //image
@@ -46,6 +47,7 @@ const Home = () =>{
             
         ))}
     </Grid>
+    <Spinner/>
     </>
     )
 }
