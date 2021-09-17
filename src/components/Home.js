1800dc1bd3c3f01657a3ccd340650bc2c61215c9
unknown <galaxy.net0710@gmail.com>
2021-09-17 13:39:12 -0500

    Button load more added

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,7 @@ import Grid  from "./Grid"
 import Thumb from "./Thumb"
 import Spinner from "./Spinner";
 import SearchBar from "./SearchBar";
+import Button from "./Button"
 //hook
 import {useHomeFetch} from '../hooks/useHomeFetch'
 //image
@@ -17,8 +18,10 @@ import NoImage from '../images/no_image.jpg'
 
 const Home = () =>{
     
-    const {state,loading,error,setSearchTerm,searchTerm}= useHomeFetch()
+    const {state,loading,error,setSearchTerm,searchTerm,setIsLoadingMore}= useHomeFetch()
     console.log(state )
+
+    if(error) return <div>Something went wrong... :(</div>
    
     return (
     <>
@@ -48,7 +51,10 @@ const Home = () =>{
             
         ))}
     </Grid>
-    <Spinner/>
+    {loading && <Spinner/>}
+    {state.page < state.total_pages && !loading && (
+        <Button text='Load More' callback={()=> setIsLoadingMore(true)} />
+    )}
     </>
     )
 }
--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -13,7 +13,9 @@ export const useHomeFetch = () =>{
     const[searchTerm,setSearchTerm]= useState('');
     const[state,setState]= useState(initialState);
     const[loading,setLoading]= useState(false);
-    const[error,setError]= useState(false)
+    const[error,setError]= useState(false);
+    const[isLoadingMore,setIsLoadingMore] = useState(false)
+
 
     console.log(searchTerm)
 
@@ -42,6 +44,13 @@ export const useHomeFetch = () =>{
         setState(initialState)
         fetchMovies(1,searchTerm)
     }, [searchTerm])
+
+    //load more
+    useEffect(() => {
+        if(!isLoadingMore) return;
+        fetchMovies(state.page + 1, searchTerm)
+        setIsLoadingMore(false);
+    }, [isLoadingMore, searchTerm, state.page])
    
-    return {state,loading,error,setSearchTerm,searchTerm}
+    return {state,loading,error,setSearchTerm,searchTerm, setIsLoadingMore}
 }
